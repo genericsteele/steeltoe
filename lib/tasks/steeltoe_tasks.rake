932ba24c3c02d0bae8f8a8099f9884f2a402cabe
Eric Steele <eric@iamericsteele.com>
2014-03-15 12:01:26 -0400

    Convert to engine

---
+++ lib/tasks/steeltoe_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :steeltoe do
+#   # Task goes here
+# end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '3e3d235603943459ab01dfd62f939dbf154d505b91f92f4eb074a475ca0d214db85a23701ea0c7d7f103ad32b864b36292b9ebbe7f66393e738e80af71002648'
